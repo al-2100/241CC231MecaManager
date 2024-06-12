d7bd8ff52c3b583b829457ecc2cbf010341c26d2
Janio Zapata <janiopi@pop-os.localdomain>
2024-06-11 19:01:24 -0500

    First commit

--- src/main/java/uni/isw/Cliente/Cliente.java
+++ src/main/java/uni/isw/Cliente/Cliente.java
@@ -1,4 +1,124 @@
 package uni.isw.Cliente;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Clientes")
 public class Cliente {
+    @Id
+    @SequenceGenerator(
+            name = "client_sequence",
+            sequenceName = "client_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "client_sequence"
+    )
+    private Long id;
+    @Column(name="dni",unique=true)
+    private String dni;
+    @Column(name="nombres")
+    private String nombres;
+    @Column(name="apellidos")
+    private String apellidos;
+    @Column(name="telefono")
+    private String telefono;
+    @Column(name="direccion")
+    private String direccion;
+    @Column(name="correo")
+    private String correo;
+
+    public Cliente() {
+
+    }
+
+    public Cliente(Long id, String dni, String nombres, String apellidos, String telefono, String direccion, String correo) {
+        this.id = id;
+        this.dni = dni;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.correo = correo;
+    }
+
+    public Cliente(String dni, String nombres, String apellidos, String telefono, String direccion, String correo) {
+        this.dni = dni;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.correo = correo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", dni='" + dni + '\'' +
+                ", nombres='" + nombres + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", correo='" + correo + '\'' +
+                '}';
+    }
 }
+
--- src/main/java/uni/isw/Cliente/ClienteRepository.java
+++ src/main/java/uni/isw/Cliente/ClienteRepository.java
@@ -1,4 +1,8 @@
 package uni.isw.Cliente;
 
-public class ClienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
